a7829a445b0b44d38e92bcff9472911008f2f04b
Vlad Eliseev <freshymanchan@gmail.com>
2020-03-18 04:04:05 +1000

    Setup basic app structure
    Configure store
    
    Create components

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Provider } from 'react-redux';
+import store from './redux/redux-store';
+import Form from './components/Form';
+import './App.scss';
+import Result from './components/Result';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <Form />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12">
+            <Result />
+          </div>
+        </div>
+      </div>
+    </Provider>
   );
 }
 
---
+++ src/components/Result/Result.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Result: React.FC = () => {
+  return (
+    <div>
+      <ul>
+        <li><b>40 000 р.</b> сотрудник будет получать на руки</li>
+        <li><b>5977 р.</b> НДФЛ, 13% от оклада</li>
+        <li><b>45 977 р.</b> за сотрудника в месяц</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Result
